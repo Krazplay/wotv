de741cd2512678ddd891519d6b349b14563112c7
Krazplay <krazplay@gmail.com>
2021-03-16 16:05:27 +0100

    Esper table update

---
+++ kraz-wotv-parser.js
@@ -0,0 +1,212 @@
+/*
+	*wotv-parser*
+	Contain variables and fonctions for parsing skills/buffs to text
+	This need to be updated when new effects are released
+*/
+
+//todo rescan buffs and check missing types
+// from ruby code 
+var typetxt = []
+typetxt[1] = "HP"
+typetxt[2] = "TP"
+typetxt[3] = "AP"
+typetxt[4] = "CT"
+typetxt[21] = "ATK"
+typetxt[22] = "DEF"
+typetxt[23] = "MAG"
+typetxt[24] = "SPR"
+typetxt[25] = "DEX"
+typetxt[26] = "AGI"
+typetxt[27] = "LUCK"
+typetxt[28] = "Move"
+typetxt[29] = "Jump"
+typetxt[41] = "None" // Elmt for No elmt (Odin, Bahamut...)
+typetxt[42] = "Fire"
+typetxt[43] = "Ice"
+typetxt[44] = "Wind"
+typetxt[45] = "Earth"
+typetxt[46] = "Lightning"
+typetxt[47] = "Water"
+typetxt[48] = "Light"
+typetxt[49] = "Dark"
+typetxt[50] = "All Elements"
+typetxt[60] = "All Attack Types"
+typetxt[61] = "Slash"
+typetxt[62] = "Pierce"
+typetxt[63] = "Strike"
+typetxt[64] = "Missile"
+typetxt[65] = "Magic"
+typetxt[81] = "Regen"
+typetxt[84] = "Poison"
+typetxt[85] = "Blind"
+typetxt[86] = "Sleep"
+typetxt[87] = "Silence"
+typetxt[88] = "Paralysis"
+typetxt[89] = "Confusion"
+typetxt[90] = "Charm"
+typetxt[91] = "Petrify"
+typetxt[92] = "Toad"
+typetxt[95] = "Haste"
+typetxt[96] = "Slow"
+typetxt[97] = "Stop"
+typetxt[98] = "Stun"
+typetxt[99] = "Immobilize"
+typetxt[100] = "Disable"
+typetxt[101] = "Berserk"
+typetxt[102] = "Doom"
+typetxt[103] = "Revive"
+typetxt[105] = "Protect"
+typetxt[106] = "Shell"
+typetxt[110] = "Float"
+typetxt[112] = "Quicken"
+typetxt[113] = "Survive Fatal hit"
+typetxt[114] = "Evade Physical hit"
+typetxt[115] = "Evade Magical hit"
+//typetxt[116] = "Conditional buff" special, va de pair avec le paramètre conds ?
+typetxt[117] = "Next hit is Crit"
+typetxt[119] = "" // Element Eater, require a tag
+typetxt[120] = "" // Type Killer, require a tag
+typetxt[122] = "" // Type Killer - Cette version est uniquement sur les 3 Master Ability
+typetxt[123] = "Proc buff" // proc another buff param id1
+typetxt[124] = "CT Up/Down"
+typetxt[134] = "Flat Dmg" // Flat Damage Type Killer
+typetxt[140] = "Poison, Blind, Sleep, Silence, Paralysis, Confusion, Petrify, Toad, Immobilize, Disable, Berserk, Stun" // Esuna
+typetxt[142] = "All buffs" // Dispel Counter, Erase
+typetxt[144] = "Male Killer?"
+typetxt[151] = "Initial AP"
+typetxt[152] = "Range"
+typetxt[155] = "ACC"
+typetxt[156] = "EVA"
+typetxt[157] = "CRIT damage"
+typetxt[158] = "CRIT"
+typetxt[159] = "CRIT EVA"
+typetxt[180] = "Hate"
+typetxt[181] = "Brave"
+typetxt[182] = "Faith"
+typetxt[183] = "Skill CT Req"
+typetxt[190] = "Acquired AP"
+typetxt[191] = "Evoc Gauge Boost"
+typetxt[192] = "Brave (temp)"
+typetxt[193] = "Faith (temp)"
+typetxt[194] = "Acquired JP"
+typetxt[200] = "Debuff Res"
+typetxt[202] = "ATK debuff Res"
+typetxt[300] = "Self-cast Buff duration"
+typetxt[301] = "Self-cast Debuff duration"
+typetxt[310] = "Unit Attack Res"
+typetxt[311] = "AoE Attack Res"
+typetxt[313] = "Evocation" // Evocation magic for esper, no one cares if I remove magic (annoying for filtering)
+typetxt[314] = "Def Penetration"
+typetxt[316] = "AP Cost Reduction"
+typetxt[329] = "Magic Res Pen"
+typetxt[347] = "Healing Power"
+
+//todo recheck killers
+var tagtxt = []
+tagtxt[2] = "Fire Eater"
+tagtxt[3] = "Ice Eater"
+tagtxt[4] = "Wind Eater"
+tagtxt[5] = "Earth Eater"
+tagtxt[6] = "Lightning Eater"
+tagtxt[7] = "Water Eater"
+tagtxt[8] = "Light Eater"
+tagtxt[9] = "Dark Eater"
+tagtxt[101] = "Man Eater"
+tagtxt[102] = "Esper Eater"
+tagtxt[103] = "Beast Killer"
+tagtxt[104] = "Demon Killer"
+tagtxt[105] = "Dragon Killer"
+tagtxt[106] = "Plantoid Killer"
+tagtxt[107] = "Avian Killer"
+tagtxt[109] = "Aquatic Killer"
+tagtxt[110] = "Machine Killer"
+tagtxt[111] = "Spirit Killer"
+tagtxt[112] = "Reaper Killer"
+tagtxt[113] = "Stone Killer"
+tagtxt[114] = "Metal Killer"
+tagtxt[115] = "Magical Creature? Killer"
+tagtxt[204] = "Fennes Killer"
+
+
+function bufflist_to_txt(buff_list) {
+	let result = ""
+	buff_list.forEach((buff_obj) => {
+		result += buff_to_txt(buff_obj);
+		result += ", ";
+	});
+	
+	result = result.slice(0,-2);
+	
+	return result;
+}
+
+function buff_to_txt(buff_obj) {
+	let result = ""
+	
+	for (let i=1; buff_obj["type"+i] != null ; i++) {
+		result += effect_to_txt(buff_obj, i);
+	}
+	
+	return result;
+}
+
+function effect_to_txt(buff_obj, nb) {
+	let output = ""
+	let type = buff_obj["type"+nb]
+	let calc = buff_obj["calc"+nb]
+	let tags = buff_obj["tag"+nb]
+	let valmin = buff_obj["val"+nb]
+	let valmax = buff_obj["val1"+nb]
+	
+	let type_str = typetxt[type];
+    // val: no need to show min and max if identical, add + if value is positive (Slash +15 instead of Slash 15)
+	let val_str = null
+    if (valmin) {
+      if (valmin == valmax) val_str = valmin > 0 ? `+${valmin}` : `${valmin}`
+      else val_str = (valmin > 0) ? `+${valmin}/${valmax}` : `${valmin}/${valmax}`
+    }
+	// tags
+	let tags_str = null;
+	if (tags) {
+		tags_str = ""
+		tags.forEach((tag) => {
+			if (tagtxt[tag]) tags_str += `${tagtxt[tag]}+`;
+			else { tags_str += `Tag${tag}+`; console.log(`Tag ${tag} inconnu buff ${buff_obj["iname"]}`) }
+		});
+		tags_str = tags_str.slice(0,-1); // remove last +
+	}
+	
+	if (calc == 1) {
+		// calc 1 is usualy a flat bonus
+		if (tags_str) output += `${tags_str} `
+		output += `${type_str} ${val_str}`
+	} else if (calc == 2) {
+		// calc 2 is usualy a % bonus
+		if (tags_str) output += `${tags_str} `
+		output += `${type_str} ${val_str}%`
+	} else if (calc == 3) {
+		// calc 3 is used for resistance bonuses
+		if (tags_str) output += `${tags_str} `
+		output += `${type_str} Res ${val_str}%`
+	} else if (calc == 30 && type == 123) {
+		// calc 30 inflict another buff if type = 123, buff_id inflicted is in param id1
+		output = "todo";
+	} else if (calc == 30 || calc == 21) {
+		// calc 30 inflict status, calc 21 inflict poison
+		if (tags_str) output += `${tags_str} `
+		output += `${type_str}`
+		if (val_str) output += `${val_str}`
+	} else if (calc == 40) {	
+		// calc 40 status is nullified
+		//todo finish this
+		output += `Nullify ${tags_str}${type_str}`
+	} else {
+		output = "Not parsed";
+		console.log(`Not parsed: Calc ${calc} Type ${type}`);
+	}
+	
+	// For esper table only, the sp cost has been added to the buff
+	if (buff_obj["sp"]) output += ` (${buff_obj["sp"]}sp)`
+	
+	return output;
+}
--- kraz-wotv.js
+++ kraz-wotv.js
@@ -1,4 +1,9 @@
-
+/*
+	*wotv*
+	get_datatables_xxx => Create an array of objects to be used as data for datatables
+	Assume global variables with same name as file, ItemName.json => itemName, Buff.json => buff
+	Be careful when naming variables
+*/
 
 /*
 	Parse a json file from folder 'en', the key parameter value become the key in the result
@@ -11,9 +16,11 @@ function parse_AnyName(data) {
 	});
 	return mapName;
 }
+
 /*
 	The iname is not always enough to have a unique identifier, a second parameter may be needed to create a unique key
 	example: adventureAreaDropDeck, you have one object per iname per campaign, iname is not unique
+	Return a Map, so use .get(key)
 */
 function parse_AnyData(data, iname, iname2 = null) {
 	let mapData = new Map();
@@ -23,12 +30,11 @@ function parse_AnyData(data, iname, iname2 = null) {
 	return mapData;
 }
 
-
 /*
 	Require adventureAreaDropDeck, adventureDropDeckEntity, adventureAreaName, itemName
 	Return an array of objects
 */
-function get_datatable_ArenaBonusUnit() {
+function get_datatable_Adventure() {
 	result = [];
 	for (let [key, value] of adventureAreaDropDeck) {
 		value.drop.forEach((table_params) => {
@@ -59,7 +65,152 @@ function get_datatable_ArenaBonusUnit() {
 	return result;
 }
 
+/*
+	Require netherBeastAbilityBoard, unit, unitName + get_esper_buffs() + sum_of_buffs_id() + wotv-parse.js
+	Return an array of objects
+*/
+function get_datatable_NetherBeast() {
+	result = [];
+	// If ID is in Esper Ability Board, then in unit file it's an equipable esper
+	for (let [iname, item] of netherBeastAbilityBoard) {
+		let base_esper = unit.get(iname)
+		let max_awk = 1+base_esper.nb_awake_id.length;
+		// One line per awakening + one for base
+		for (awk_nb = 0; awk_nb < max_awk; awk_nb++) {
+			// Pick the right esper unit depending of the awakening
+			let esper = awk_nb == 0 ? base_esper : unit.get(base_esper.nb_awake_id[awk_nb-1])
+			//console.log(esper);
+			let line = {};
+			line["iname"] = esper.iname;
+			line["name"] = unitName[esper.iname] ? unitName[esper.iname] : esper.iname;
+			line["awk"] = awk_nb;
+			// Getting all the possible stats, status[1] is the object with max stats
+			stats_list.forEach((stat) => {
+				line[stat] = esper.status[1][stat] ? esper.status[1][stat] : "";
+			});
+			// Element(s) of esper
+			line["elem"] = ""
+			esper.elem.forEach((elemt) => {
+				line["elem"] += typetxt[41+elemt] + ", ";
+			});
+			line["elem"] = line["elem"].slice(0,-2); // remove last ", "		
+			// Get all the buffs of the esper at this awakening
+			let buff_list = get_esper_buffs(iname, awk_nb);
+			// Merge similar buffs, this is now an array of buff objects
+			buff_list = sum_of_buffs_id(buff_list);
+			// Sort array to show most useful buffs first
+			buff_list.sort((a, b) => a.sort_priority - b.sort_priority);
+			// Translate into a readable text
+			line["skills_txt"] =  bufflist_to_txt(buff_list);
+			result.push(line);
+		}
+	}
+	//console.log(result);
+	return result;
+}
+
+/*
+	Require netherBeastAbilityBoard, buff
+	Add the SP cost of the panel to the buff (as param sp)
+	Return an array of buff id available to the esper at this awakening level
+*/
+function get_esper_buffs(base_esper_iname, awk_lvl) {
+	let board = netherBeastAbilityBoard.get(base_esper_iname)["panels"];
+	let buffs_list = [];
+	board.forEach((panel) => {
+		if (panel.unlock_awake == null || awk_lvl+1 >= panel.unlock_awake) {
+			buffs_list.push(panel.value);
+			buff.get(panel.value)["sp"] = panel.sp;
+		}
+	});
+	return buffs_list;
+}
+
+/*
+	Input an array of buff ID, return an array of buff objects
+	Require buff
+	[buff_id1, buff_id2] (Slash+3, Slash+2) => [buff] (Slash+5)
+	To be safe, create a buff from scratch to make sure we don't clone multiple types by error
+	Also add sort_priority to easily sort them later if needed
+*/
+function sum_of_buffs_id(buff_list) {
+	//console.log(buff_list);
+	let result = new Map;
+	buff_list.forEach((buff_id) => {
+		let curBuff = buff.get(buff_id);
+		// Loop as long as we find valid typeX in the buff params
+		for (let i=1; curBuff["type"+i] != null ; i++) {
+			// If the type, calc, and tags match, we consider it's the same kind of buff
+			let key = 'Type '+curBuff["type"+i]+' calc '+curBuff["calc"+i]+' tags '+curBuff["tag"+i];
+			// First time we meet this kind of buff
+			if (result.has(key) == false) {
+				let new_buff = clone_buff_minus_type(curBuff);
+				// copy type and val X to 1 in the new buff 
+				let param_list = ["type", "calc", "tag", "val", "val1"];
+				param_list.forEach((param) => {
+					new_buff[param+"1"] = curBuff[param+i];
+				});
+				// For easy sorting later if needed
+				new_buff["sort_priority"] = calculate_sort_buff(new_buff);
+				result.set(key, new_buff);
+			}
+			// Else we just add the value to the existing one
+			else {
+				result.get(key)["val1"] += curBuff["val"+i];
+				result.get(key)["val11"] += curBuff["val1"+i];
+				result.get(key)["sp"] += curBuff["sp"];
+			}
+		}
+	});
+	// Return only the values as an array, don't care for the keys
+	return Array.from(result.values());
+}
+
+/*
+	Clone the parameters except everything to do with the type (calc, tags, val, val1, etc...)
+	Easier for maintenance if they add new params
+*/
+function clone_buff_minus_type(buff_obj) {
+	let result = {};
+	let param_list = ["iname", "rate", "turn", "timing", "chktgt", "chektiming", "conds", "continue", "sp"];
+	param_list.forEach((param) => {
+		result[param] = buff_obj[param];
+	});
+	return result;
+}
+
+/*
+	Gives a sort order to a buff
+	For the espers long list of buffs, I prefer to sort the buff by effect
+*/
+function calculate_sort_buff(buff_obj) {
+	if (buff_obj["type1"] >= 61 && buff_obj["type1"] <= 65 && buff_obj["calc1"] != 3) return 1; // Dmg type
+	if (buff_obj["type1"] >= 42 && buff_obj["type1"] <= 60 && buff_obj["calc1"] != 3) return 2; // Elements
+	if (buff_obj["type1"] >= 21 && buff_obj["type1"] <= 26) return 3;  // Stats
+	if (buff_obj["type1"] >= 1 && buff_obj["type1"] <= 4) return 5;  // HP TP AP +%
+	if (buff_obj["type1"] == 155) return 7;  // Accuracy
+	if (buff_obj["type1"] == 156) return 9;  // Evade
+	if (buff_obj["type1"] == 158) return 11; // Crit
+	if (buff_obj["type1"] == 159) return 13; // Crit evade
+	if (buff_obj["type1"] == 120 && buff_obj["calc1"] == 30) return 15; // Type killer
+	if (buff_obj["type1"] == 119 && buff_obj["calc1"] == 30) return 17; // Elt eater
+	if (buff_obj["type1"] >= 61 && buff_obj["type1"] <= 65 && buff_obj["calc1"] === 3) return 19; // type res
+	if (buff_obj["type1"] == 313) return 99; // Evocation magic
+	return 50;
+}
+
 // Because javascript
 function round(value, decimals) {
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+	return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
+
+// 
+var stats_list = [
+				"hp","mp","ap",
+				"atk","mag","def","mnd","hit","avd",
+				"dex","spd","luk","crt","crta","iniap",
+				"mov","jmp","dmax",
+				"asl","api","abl","ash","ama","apl",
+				"ewi","eth","efi","eic","esh","eea","eda","ewa",
+				"cbl","csl","cmu","cch","cdo","cst","cda","cbe","cpo","cpa","ccf","cfr","cpe","cdm","csw"
+				];
