fea214e136d9d34c6f8b21839c87fe3c1a28c504
Krazplay <krazplay@gmail.com>
2021-04-10 20:38:11 +0200

    2021_04_10

---
+++ kraz-wotv-missions.js
@@ -0,0 +1,77 @@
+/*
+	*wotv-parser-missions*
+	Contain fonctions for mission maps json
+*/
+
+// Rough display of the content an object
+function object_to_html(quest_obj, indent=0) {
+	let result = "";
+	let str_ind = "";
+	for (let i=0; i < indent; i++) { str_ind += "&nbsp"; }
+	for (let key in quest_obj) {
+		result += str_ind;
+		if (typeof quest_obj[key] === 'object') {
+			result += key+":<br/>";
+			result += object_to_html(quest_obj[key], indent+6);
+		}
+		else result += key+": "+quest_obj[key]+"<br/>";
+	}
+	return result;
+}
+
+function extract_mapset_enemies_to_html(map_set_json) {
+	let result = "";
+	let list = get_mapset_enemies(map_set_json);
+	let count = {};
+	//console.log(list);
+	
+	list.forEach((enemy) => {
+		result += enemy.name + " Lv" +enemy.lv + "<br/>";
+		let elem_str = typetxt[41+enemy.elem];
+		result += elem_str + ", drop: " + enemy.drop + ", enemy id:"+enemy.iname+"<br/>";
+		if (!count[enemy.drop]) count[enemy.drop] = 0;
+		count[enemy.drop] += 1;
+		if (!count[enemy.crystal]) count[enemy.crystal] = 0;
+		count[enemy.crystal] += 1;
+	});
+	
+	result += "<br/><br/><br/>"
+	// test drop display
+	map_set_json["drop_table_list"].forEach((drop_table) => {
+		let number_drop = count[drop_table["iname"]] ? count[drop_table["iname"]] : 0;
+		result += drop_table["iname"] + " (" + number_drop + ")<br/>";
+		// Calculate sum of weight instead of assuming 10000
+		drop_table["drop_list"].forEach((drop) => {
+			if (!drop_table["total_weight"]) drop_table["total_weight"] = 0;
+			drop_table["total_weight"] += drop["weight"];
+		});
+		
+		result += "<table style='margin-left: 30px;'>";
+		result += "<thead><tr><th>Name</th><th>Amount</th><th>Weight</th><th>Drop rate</th><th>Average per mission</th></tr></thead>";
+		result += "<tbody>";
+		drop_table["drop_list"].forEach((drop) => {
+			result += "<tr>"; // new row
+			let item_name = itemName[drop["drop_data"]["iname"]] ? itemName[drop["drop_data"]["iname"]] : drop["drop_data"]["iname"];
+			result += "<td>"+item_name+"</td>"; // 1st cell item name
+			result += "<td>x"+drop["drop_data"]["num"]+"</td>"; // 2nd cell amount of drop
+			result += "<td>"+drop["weight"]+"</td>"; // 3rd cell weight
+			result += "<td>"+round(100*drop["weight"]/drop_table["total_weight"], 3)+"%</td>"; // 4th cell calculated drop rate
+			result += "<td>"+round(number_drop*drop["weight"]/drop_table["total_weight"], 4)+"</td>"; // 5th cell average drop per mission
+			result += "</tr>"; // end row
+		});
+		result += "</tbody></table>";
+		result += "&emsp;&emsp;- Total weight= " + drop_table["total_weight"] + "<br/><br/>";
+	});
+	
+	return result;
+}
+
+function get_mapset_enemies(map_set_json) {
+	let result = [];
+	map_set_json["enemy"].forEach((enemy) => {
+		if (!enemy.name) enemy.name = unitName[enemy.iname] ? unitName[enemy.iname] : enemy.iname;
+		result.push(enemy);
+	});
+	//console.log(result);
+	return result;
+}
--- kraz-wotv.js
+++ kraz-wotv.js
@@ -14,7 +14,26 @@ function parse_AnyName(data) {
 	data.infos.forEach((info) => {
 		if (info.value) mapName[info.key] = info.value;
 	});
+	quicktest_add_jp_names(mapName);
 	return mapName;
+}
+
+function quicktest_add_jp_names(mapName) {
+	if (mapName["UN_LW_P_MONT"]) {
+		mapName["UN_LW_P_LILS_01"] = mapName["UN_LW_P_LILS_01"] || "Lilyth Swimsuit (JP)"
+		mapName["UN_LW_P_KTON_01"] = mapName["UN_LW_P_KTON_01"] || "Kitton Swimsuit (JP)"
+		mapName["UN_NIER_P_N2TB"] = mapName["UN_NIER_P_N2TB"] || "2B (JP)"
+		mapName["UN_NIER_P_N9TS"] = mapName["UN_NIER_P_N9TS"] || "9S (JP)"
+		mapName["UN_LW_P_MONT_01"] = mapName["UN_LW_P_MONT_01"] || "King Mont (JP)"
+		mapName["UN_LW_P_RYEL"] = mapName["UN_LW_P_RYEL"] || "Ruel/Louelle (JP)"
+		mapName["UN_LW_P_SIRM"] = mapName["UN_LW_P_SIRM"] || "Sylma? (JP)"
+		mapName["UN_LW_P_HLNA_01"] = mapName["UN_LW_P_HLNA_01"] || "Helena Black Witch (JP)"
+		mapName["UN_LW_P_THLA_01"] = mapName["UN_LW_P_THLA_01"] || "Salire Valentine (JP)"
+		mapName["UN_LW_P_CMLO"] = mapName["UN_LW_P_CMLO"] || "Camillo (JP)"
+		mapName["UN_LW_P_MORE"] = mapName["UN_LW_P_MORE"] || "Moore (JP)"
+		mapName["UN_LW_P_CWEL"] = mapName["UN_LW_P_CWEL"] || "Cowell (JP)"
+		mapName["UN_LW_P_CRLT"] = mapName["UN_LW_P_CRLT"] || "Charlotte (JP)";
+	}
 }
 
 // In case we want an easily iterable variable of a name file
@@ -219,7 +238,6 @@ function get_datatable_Artifact() {
 	result = [];
 	
 	let typeName = ["Weapon","Armor","Accessory", "-1"];
-	let rareName = ["N","R","SR","MR","UR"];
 	// todo replace catName with file data
 	let catName = ["0","Dagger","Sword","Greatsword","Katana","Staff","Ninja Blade","Bow","Axe", "HammerNotUsed",
 					"Spear","InstrumentNotUsed","WhipNotUsed","ProjectileNotUsed","Gun","Mace","Fists","Shield","Armor","Hat",
@@ -246,6 +264,10 @@ function get_datatable_Artifact() {
 			line["equip"] = equip.equip ? equip.equip : "";
 			line["rtype"] = equip.rtype;
 			line["grow"] = grow_id;
+			line["bestv"] = 0;
+			let plus_one = equip.iname.charCodeAt(equip.iname.length-1) + 1;
+			let test = equip.iname.slice(0, equip.iname.length-2)+"_"+String.fromCharCode(plus_one);
+			if (!artifact.get(equip.iname+"_1") && !artifact.get(test)) line["bestv"] = 1;
 			let curr_grow = grow.get(grow_id);
 			let curve = curr_grow["curve"][0];
 			// Getting all the possible stats, status[1] is the object with max stats
@@ -316,8 +338,8 @@ function get_datatable_Unit() {
 		let line = {};
 		line["iname"] = object.iname;
 		line["name"] = unitName[object.iname] ? unitName[object.iname] : object.iname;
-		
 		line["sex"] = object.sex ? object.sex : "";
+		line["rare"] = object.rare ? rareName[object.rare] : "";
 		line["elem"] = elem_to_text(object.elem);
 		line["species"] = species_to_text(object.species);
 		
@@ -384,103 +406,41 @@ function get_datatable_Unit_wjobs() {
 		let line = {};
 		line["iname"] = object.iname;
 		line["name"] = unitName[object.iname] ? unitName[object.iname] : object.iname;
+		line["sex"] = object.sex ? object.sex : "";
+		line["rare"] = object.rare ? rareName[object.rare] : "";
+		line["elem"] = elem_to_text(object.elem);
+		line["species"] = species_to_text(object.species);
+		
 		line["PC"] = (object.PC) ? object.PC : "";
 		line["Board"] = "";
 		// Status => [{Lv1}, {Lv99}, {Lv120}]
 		line["lvl"] = "";
 		if (object.status) {
-			for (let i=1; object.status[i]; i++) {
-				let stats_obj = object.status[i];
-				// Clone the existing line
-				let line_2 = Object.assign({}, line);
-				line_2["lvl"] = ["1","99","120"][i];
-				// Get the jobs % stats bonuses
-				job_bonus = {};
-				// If the unit has jobs, grab bonus rates of all jobs
-				if (object.jobsets) {
-					object.jobsets.forEach((job_id, index) => {
-						job_obj = job.get(job_id);
-						// First job bonus is 100%, else use the sub_rate (always 50% so far)
-						let rate = (index == 0) ? 100 : job_obj["sub_rate"]
-						// Sometimes we don't have full stats in a job, we only have them in the 'origin' job
-						let job_origin = job.get(job_obj["origin"]);
-						// If no val for iniap in unit, use the main job one // todo check EX jobs
-						if (index == 0 && stats_obj["iniap"] == null) {
-							if (job_obj["ranks"][14]) stats_obj["iniap"] = job_obj["ranks"][14]["iniap"];
-							else stats_obj["iniap"] = job_origin["ranks"][14]["iniap"];
-						}
-						
-						stats_list.forEach((stat) => {
-							if (job_bonus[stat] == null) job_bonus[stat] = 0; // init
-							// EX job replace the old job, check if level>=120, it's main job, and ccsets exist
-							if (i>1 && index == 0 && object["ccsets"]) {
-								let exjob_obj = job.get(object["ccsets"][0]["m"]);
-								// Tricky, if EX stats are missing, check the job in param 'origin'
-								if (exjob_obj["ranks"][9] == null) {
-									exjob_obj = job.get(exjob_obj["origin"]);
-								}
-								// If the stat rate bonus exist, add it to job_bonus
-								if (exjob_obj["ranks"][9][stat]) {
-									job_bonus[stat] += exjob_obj["ranks"][9][stat] * rate / 100;
-								}
-							}
-							else {
-								//console.log(iname+"  "+job_id);
-								// If the stat rate bonus exist, add it to job_bonus
-								if (job_obj["ranks"][14] && job_obj["ranks"][14][stat]) {
-									job_bonus[stat] += job_obj["ranks"][14][stat] * rate / 100;
-								}
-								// Once again trick, stat may be only in origin job
-								else if (job_origin && job_origin["ranks"][14][stat]) {
-									job_bonus[stat] += job_origin["ranks"][14][stat] * rate / 100;
-								}
-							}
-						});
-					});
-				}
-				// Calculate the stats
+			let stats = get_unit_stats(iname);
+			for (let i=1; unit_obj.status[i]; i++) {
+				line["lvl"] = ["1","99","120"][i];
+				let line_s = Object.assign({}, line);
+				let line_sb = Object.assign({}, line);
+				line_s["Board"] = 0;
+				line_sb["Board"] = 1;
+				
+				// Stats
 				stats_list.forEach((stat) => {
-					if (job_bonus[stat] == null) job_bonus[stat] = 0;
-					if (stats_obj[stat]) {
-						line_2[stat] = stats_obj[stat];
-						line_2[stat] += Math.floor(stats_obj[stat] * job_bonus[stat] / 10000);
+					if (stats[i]["base"][stat]) {
+						line_s[stat] = stats[i]["base"][stat];
 					}
-					else { line_2[stat] = ""; }
+					else { line_s[stat] = ""; }
+					
+					if ( object.PC && (stats[i]["base"][stat] || stats[i]["board"][stat]) ) {
+						let base = stats[i]["base"][stat] ? stats[i]["base"][stat] : 0;
+						let board = stats[i]["board"][stat] ? stats[i]["board"][stat] : 0;
+						line_sb[stat] = base + board;
+					}
+					else { line_sb[stat] = ""; }
 				});
 				
-				// This is the line for base_stats (lvl stats + job stats)
-				result.push(line_2);
-				
-				//
-				// Now if the unit has a board, we're going to create another line adding board stats
-				//
-				if (object.PC) {
-					// Clone the base stats line
-					let line_3 = Object.assign({}, line_2);
-					line_2["Board"] = 0; // 0 mean he has a board, but board stats are not included
-					line_3["Board"] = 1;
-					// Assume lvl99 = job lvl 15 is max, lvl 120 => job level 25
-					let board_job_level = [0,15,25][i];
-					// Get the sum of bonuses for this job level
-					let board_bonus = get_unit_board_bonuses(object.iname, board_job_level);
-					// Add the bonuses to the base stats
-					stats_list.forEach((stat) => {
-						// % bonus
-						if (board_bonus["%"][stat]) {
-							if (!line_3[stat]) line_3[stat] = 0;
-							let base_stat = line_3[stat];
-							if (board_bonus["%"][stat]) line_3[stat] += Math.floor(board_bonus["%"][stat] * base_stat / 100);
-						}
-						// flat bonus
-						if (board_bonus["+"][stat]) {
-							if (!line_3[stat]) line_3[stat] = 0;
-							let base_stat = line_3[stat];
-							if (board_bonus["+"][stat]) line_3[stat] += board_bonus["+"][stat];
-						}
-					});
-					
-					result.push(line_3);
-				}
+				result.push(line_s);
+				if (object.PC) result.push(line_sb);
 			}
 		}
 		else {
@@ -583,7 +543,6 @@ function add_new_stats(origin, new_stats) {
 }
 
 function get_datatable_VisionCard() {
-	let rareName = ["N","R","SR","MR","UR"];
 	let tmaxlvl =  [30, 40, 60, 70, 99]
 	let tlvlawa =  [[10, 15, 20, 25, 30],
 					[20, 25, 30, 35, 40],
@@ -712,7 +671,6 @@ function get_skill_buff_at_level(skill_obj, skill_lvl) {
 		// todo add ceil for negative values
 		buff_result["val"+i] = Math.floor( base_val + ( max_gain * (skill_lvl-start_lvl) / (lvl_max-start_lvl) ) );
 		buff_result["val"+i+"1"] = Math.floor( base_val + ( max_gain * (skill_lvl-start_lvl) / (lvl_max-start_lvl) ) );
-		//console.log(`Base=${base_val}, Max gain=${max_gain}, skill_lvl=${skill_lvl}, lvl_max=${lvl_max}, result=${buff_result["val"+i]}`);
 	}
 	
 	return buff_result;
@@ -755,22 +713,135 @@ function get_datatable_Skill_charac() {
 		let board = unitAbilityBoard.get(iname);
 		// If has a board
 		if (board) {
-			skill_list = get_unit_board_skills(iname, 25);
+			let skill_list = get_unit_board_skills(iname, 25);
+			let full_stats = get_unit_stats(iname);
+			let stats = full_stats[full_stats.length - 1]; // use highest stats available
+			
 			skill_list.forEach((skill_id) => {
 				let line = {};
+				let sk = skill.get(skill_id);
 				line["iname"] = iname;
 				line["name"] = unitName[iname] ? unitName[iname] : iname;
 				line["skill_id"] = skill_id;
 				line["skill_name"] = skillName[skill_id] ? skillName[skill_id] : skill_id;
+				line["slot"] = sk.slot ? sk.slot : ""; // castable, passive, counter, etc...
+				line["cap"] = sk.cap ? sk.cap : "";    // Level cap
+				line["count"] = sk.count ? sk.count : ""; // Number of cast per battle
+				line["eff_val"] = sk.eff_val ? sk.eff_val+100 : "";       // Multiplier at level 1
+				line["eff_val1"] = sk.eff_val1 ? sk.eff_val1+100 : "";    // Multiplier at max level
+				line["cost_type"] = sk.cost_type ? sk.cost_type : ""; // Cost type (AP, TP...)
+				line["cost_ap"] = sk.cost_ap ? sk.cost_ap : "";
+				line["cost_mp"] = sk.cost_mp ? sk.cost_mp : "";
+				line["hp_cost"] = sk.hp_cost ? sk.hp_cost : "";
+				line["timing"] = sk.timing ? sk.timing : "";
+				
+				// Damage type
+				line["atk_type"] = "";
+				if (sk["atk_type"] != null) {
+					let dmg_array = ["-", "Physical", "Magical", "Hybrid<br/>See FAQ"];
+					if (dmg_array[sk["atk_type"]]) line["atk_type"] += dmg_array[sk["atk_type"]];
+					else {
+						line["atk_type"] += sk["atk_type"];
+						console.log("Skill "+line["skill_name"]+" ("+line["name"]+") atk_type: "+sk["atk_type"]);
+					}
+				}
+				// Attack type
+				line["atk_det"] = "";
+				if (sk["atk_det"] != null) {
+					let atk_array = ["-", "Slash atk", "Pierce atk", "Strike atk", "Missile atk", "Magic atk"];
+					if (atk_array[sk["atk_det"]]) line["atk_det"] += atk_array[sk["atk_det"]];
+					else {
+						line["atk_det"] += sk["atk_det"];
+						console.log("Skill "+line["skill_name"]+" ("+line["name"]+") atk_det: "+sk["atk_det"]);
+					}
+				}
+				
+				// Main stat for calculating the skill power
+				if (sk.atk_base) {
+					if (atk_base[sk.atk_base]) line["atk_base"] = atk_base[sk.atk_base];
+					else {
+						line["atk_base"] = sk.atk_base;
+						console.log("Skill "+line["skill_name"]+" ("+line["name"]+") atk_base "+sk.atk_base+" not translated");
+					}
+				}
+				else line["atk_base"] = "";
+				// Sub stats for calculating the skill power
+				if (sk.atk_formula != null) {
+					line["atk_formula"] = "";
+					if (sk.atk_formula_t4) console.log("Warning atk_formula_t4 now exist");
+					let formula_stat = [ [null, "DEX", "AGI", "LUCK"],
+										 [null, "ATK"] ];
+					for (let i=0; i<4; i++) {
+						let atk_formula_tx = "atk_formula_t"+i;
+						if (sk[atk_formula_tx]) {
+							line["atk_formula"] += sk[atk_formula_tx]+"% "+formula_stat[sk.atk_formula][i]+"<br/>";
+						}
+					}
+				}
+				else line["atk_formula"] = "";
+				
+				// Stat power of the character for this skill
+				line["stats_pow"] = ""
+				if (sk.atk_base) {
+					line["stats_pow"] = 0;
+					if (atk_base_param[sk.atk_base]) line["stats_pow"] += stats["total"][atk_base_param[sk.atk_base]];
+				}
+				// From my memory the bonus for substats is truncated at each stats
+				if (sk.atk_formula != null) {
+					if (!line["stats_pow"]) line["stats_pow"] = 0;
+					let formula_stat = [ [null, "dex", "spd", "luk"],
+										 [null, "atk"] ];
+					for (let i=0; i<4; i++) {
+						let atk_formula_tx = "atk_formula_t"+i;
+						if (sk[atk_formula_tx]) {
+							line["stats_pow"] += Math.floor( sk[atk_formula_tx] * stats["total"][formula_stat[sk.atk_formula][i]] / 100 );
+						}
+					}
+				}
+				
+				// Quick range
+				line["range"] = "";
+				if (sk["range_s"] != null || sk["range_l"] != null || sk["range_w"] != null || sk["range_h"] != null || sk["range_mh"] != null || sk["range_bns"] != null) {
+					if (sk["range_s"] != null) line["range"] += `S${sk["range_s"]}`
+					if (sk["range_l"] != null) line["range"] += `L${sk["range_l"]}`
+					if (sk["range_w"] != null) line["range"] += `W${sk["range_w"]}`
+					if (sk["range_h"] != null) line["range"] += `H${sk["range_h"]}`
+					if (sk["range_mh"] != null) line["range"] += ` MH${sk["range_mh"]}`
+					if (sk["range_bns"] != null) line["range"] += ` BNS${sk["range_bns"]}`
+				}
+				
+				line["range_eff"] = "";
+				if (sk["eff_s"] != null || sk["eff_l"] != null || sk["eff_w"] != null || sk["eff_h"] != null || sk["eff_mh"] != null || sk["eff_bns"] != null) {
+					if (sk["eff_s"] != null) line["range_eff"] += `S${sk["eff_s"]}`
+					if (sk["eff_l"] != null) line["range_eff"] += `L${sk["eff_l"]}`
+					if (sk["eff_w"] != null) line["range_eff"] += `W${sk["eff_w"]}`
+					if (sk["eff_h"] != null) line["range_eff"] += `H${sk["eff_h"]}`
+					if (sk["eff_mh"] != null) line["range_eff"] += ` MH${sk["eff_mh"]}`
+					if (sk["eff_bns"] != null) line["range_eff"] += ` BNS${sk["eff_bns"]}`
+				}
+				
 				result.push(line);
 			});
 		}
 	}
 	return result;
 }
+atk_base = [];
+atk_base[1] = "ATK";
+atk_base[3] = "MAG";
+atk_base[52] = "Max HP";
+atk_base[56] = "Max TP";
+atk_base[60] = "Max AP";
+atk_base[102] = "Dmg taken";
 
-		// new effect type 3 => damage max up maybe -not checked-
-		// new effect type 4 => UnitMaterialItem
+atk_base_param = [];
+atk_base_param[1] = "atk";
+atk_base_param[3] = "mag";
+atk_base_param[52] = "hp";
+atk_base_param[56] = "mp";
+atk_base_param[60] = "ap";
+
+
 function get_unit_board_skills(unit_iname, job_level) {
 	let result = [];
 	
@@ -780,12 +851,126 @@ function get_unit_board_skills(unit_iname, job_level) {
 	board["panels"].forEach((panel) => {
 		// If panel require a job lvl less or equal our parameter and is not a castable skill
 		// Skill for damage max up, we handle it
-		if (panel["need_level"] <= job_level && panel["panel_effect_type"] == 1) {
+		if ( (!panel["need_level"] || panel["need_level"] <= job_level) && panel["panel_effect_type"] == 1) {
 			result.push(panel["value"]);
 		}
 	});
 	
 	return result;
+}
+
+// Get level stats 1, 99, and 120, then if lvl 99 or 120, add job_bonus stats
+function get_unit_stats(unit_iname) {
+	rstats = []; // Result, an array of stats hashes [{lv1},{lv99},{lv120}]
+	unit_obj = unit.get(unit_iname);
+	
+	// Must have status, units like chests don't have one
+	if (unit_obj.status) {
+		// Status => [{Lv1}, {Lv99}, {Lv120}]
+		for (let i=0; unit_obj.status[i]; i++) {
+			if (!rstats[i]) rstats[i] =	{};
+			if (!rstats[i]["base"]) rstats[i]["base"] =	{};
+			if (!rstats[i]["total"]) rstats[i]["total"] =	{};
+			rstats[i]["base"]["lvl"] = ["1","99","120"][i];
+			// Easier name for current level stats
+			let lvl_stats = unit_obj.status[i];
+			// Get the jobs % stats bonuses
+			job_bonus = {};
+			// If the unit has jobs, grab bonus rates of all jobs, ignore for level 1
+			if (unit_obj.jobsets && i>0) {
+				unit_obj.jobsets.forEach((job_id, index) => {
+					job_obj = job.get(job_id);
+					// First job bonus is 100%, else use the sub_rate (always 50% so far)
+					let rate = (index == 0) ? 100 : job_obj["sub_rate"]
+					// Sometimes we don't have full stats in a job, we only have them in the 'origin' job
+					let job_origin = job.get(job_obj["origin"]);
+					// If no val for iniap in unit, use the main job one // todo check EX jobs
+					if (index == 0 && lvl_stats["iniap"] == null) {
+						if (job_obj["ranks"][14]) lvl_stats["iniap"] = job_obj["ranks"][14]["iniap"];
+						else lvl_stats["iniap"] = job_origin["ranks"][14]["iniap"];
+					}
+					// For each stat, calculate the sum of job % bonus
+					stats_list.forEach((stat) => {
+						if (job_bonus[stat] == null) job_bonus[stat] = 0; // init
+						// EX job replace the old job, check if level>=120, it's main job, and ccsets exist
+						if (i>1 && index == 0 && unit_obj["ccsets"]) {
+							let exjob_obj = job.get(unit_obj["ccsets"][0]["m"]);
+							// Tricky, if EX stats are missing, check the job in param 'origin'
+							if (exjob_obj["ranks"][9] == null) {
+								exjob_obj = job.get(exjob_obj["origin"]);
+							}
+							// If the stat rate bonus exist, add it to job_bonus
+							if (exjob_obj["ranks"][9][stat]) {
+								job_bonus[stat] += exjob_obj["ranks"][9][stat] * rate / 100;
+							}
+						}
+						else {
+							// If the stat rate bonus exist, add it to job_bonus
+							if (job_obj["ranks"][14] && job_obj["ranks"][14][stat]) {
+								job_bonus[stat] += job_obj["ranks"][14][stat] * rate / 100;
+							}
+							// Once again trick, stat may be only in origin job
+							else if (job_origin && job_origin["ranks"][14][stat]) {
+								job_bonus[stat] += job_origin["ranks"][14][stat] * rate / 100;
+							}
+						}
+					});
+				});
+			}
+			// Calculate the stats
+			stats_list.forEach((stat) => {
+				if (job_bonus[stat] == null) job_bonus[stat] = 0;
+				if (lvl_stats[stat]) {
+					rstats[i]["base"][stat] = lvl_stats[stat];
+					rstats[i]["base"][stat] += Math.floor(lvl_stats[stat] * job_bonus[stat] / 10000);
+				}
+				else { rstats[i]["base"][stat] = ""; }
+				rstats[i]["total"][stat] = rstats[i]["base"][stat];
+			});
+				
+				
+			//---------------------------
+			// Board stats
+			//---------------------------
+			if (unit_obj.PC && i>0) {
+				// init
+				if (!rstats[i]["board"]) rstats[i]["board"] = {};
+				// Assume lvl99 = job lvl 15 is max, lvl 120 => main job level 25
+				let board_job_level = [0,15,25][i];
+				// Get the sum of bonuses for this job level
+				let board_bonus = get_unit_board_bonuses(unit_obj.iname, board_job_level);
+				// Add the bonuses to the hash "board"
+				stats_list.forEach((stat) => {
+					// % bonus
+					if (board_bonus["%"][stat]) {
+						if (!rstats[i]["board"][stat]) rstats[i]["board"][stat] = 0;
+						let base_stat = rstats[i]["base"][stat];
+						if (board_bonus["%"][stat]) rstats[i]["board"][stat] += Math.floor(board_bonus["%"][stat] * base_stat / 100);
+					}
+					// flat bonus
+					if (board_bonus["+"][stat]) {
+						if (!rstats[i]["board"][stat]) rstats[i]["board"][stat] = 0;
+						let base_stat = rstats[i]["base"][stat];
+						if (board_bonus["+"][stat]) rstats[i]["board"][stat] += board_bonus["+"][stat];
+					}
+					if (rstats[i]["board"][stat]) rstats[i]["total"][stat] += rstats[i]["board"][stat];
+				});
+			}
+			
+			//---------------------------
+			// Master skill stats
+			//---------------------------
+			// todo
+			if (unit_obj.mstskl) {
+				// Get only the last master skill (when more than one, they don't stack)
+				//let mst_skl_id = unit_obj.mstskl[unit_obj.mstskl.length-1];
+				//let list_buffs = get_buffs_from_skill_id(mst_skl_id);
+				
+			}
+			
+		}
+	}
+	return rstats;
 }
 
 // Because javascript
@@ -810,3 +995,5 @@ var visions_stats_list = [
 				"atk","mag","def","mnd","hit","avd",
 				"dex","spd","luk","crt","crta"
 				];
+				
+const rareName = ["N","R","SR","MR","UR"];
